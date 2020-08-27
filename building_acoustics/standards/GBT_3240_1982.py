92cc9efb975773abf316f2d987ca9c4baf34e78e
Fengli Hu <hlf310@163.com>
2020-08-27 14:13:59 +0800

    常规—优化 频率计算

--- building_acoustics/standards/GBT_3240_1982.py
+++ building_acoustics/standards/GBT_3240_1982.py
@@ -3,23 +3,28 @@ import numpy as np
 class frequency(object):
     'GB/T 3240-1982 声学测量中的常用频率 ；GB/T 3241-1998 倍频程和分数倍频程滤波器   k=0为1000Hz，频率选择是一个整数, b=1为倍频程；b=1/3为1/3倍频程'
 
-    def __init__(self,f=-1,k=-100,b=1/3):
+    def __init__(self,f=None,k=None,b=1/3):
+        """k, 为1/3倍频程数k=0时为1000Hz，\
+           n, 为1/1倍频程数，n=0时为1000Hz"""
         self.B = b  # 频程（b=1为倍频程；b=1/3为1/3倍频程）
-        if k!=-100:self.judgement_int(k)
-        if f==-1 :
+        if f==None and k!=None:
+            self.judgement_int(k)
             self.calculate_F()
             self.calculate_BF()
-        elif f!=-1:
+        elif f!=None and k==None:
             self.BF=f
             self.calculate_K()
             self.calculate_F()
+        else:
+            # input("警告：f,k不能同时输入数据")
+            exit("警告：f,k不能同时输入数据!!!")
 
 
     def __str__(self):
         if self.B==1:
-            return  '倍频程=>准确值：%.2fHz；标称值：%.fHz'%(self.F,self.BF)
+            return  '倍频程=>准确值：%.2fHz；标称值：%.2fHz'%(self.F,self.BF)
         else:
-            return '1/3倍频程=>准确值：%.2fHz；标称值：%.fHz'%(self.F,self.BF)
+            return '1/3倍频程=>准确值：%.2fHz；标称值：%.2fHz'%(self.F,self.BF)
 
     __repr__ = __str__
 
